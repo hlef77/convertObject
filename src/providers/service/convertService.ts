6175e3d3fa6318308585891a16f408d28d7a989c
takami <hlef.wtbr.77@gmail.com>
2017-12-09 23:35:57 +0900

    add func of same tag to Array

--- src/providers/service/convertService.ts
+++ src/providers/service/convertService.ts
@@ -8,10 +8,14 @@ export class ConvertService {
 
   public parseXmlToJson(xmlTxt: string) {
 
+    // オブジェクト管理用配列
     let currentObj: Array < object > = [{}];
+    // キー名を保持する配列
     let keyNameStack: Array < string > = [];
+    // 管理中の深さを表す
     let depth: number = 0;
-    let beforeTagKind: number = 0;
+    // 現要素のひとつ前の要素種別を保持する
+    let beforeElementKind: number = 0;
 
     // 前処理：空タグを開きタグ&閉じタグに置換
     xmlTxt = xmlTxt.replace(MatchTagTextReg.EMPTY_G, '<$1></$1>');
@@ -56,7 +60,7 @@ export class ConvertService {
           // 階層を次に進める
           depth++;
           // 手前タグ種を「開きタグ」に
-          beforeTagKind = KIND.TAG_OPEN;
+          beforeElementKind = KIND.TAG_OPEN;
 
           break;
           // -----------------------------------------------------
@@ -75,19 +79,18 @@ export class ConvertService {
             if (currentObj[depth + 1] === null || currentObj[depth + 1] === undefined) {
               currentObj[depth + 1] = {}
             }
-            let tmpObj = currentObj[depth];
-            tmpObj[element.content] = currentObj[depth + 1];
+            this.addKeyAndValue(currentObj, depth, element.content, currentObj[depth + 1]);
             currentObj[depth + 1] = {};
           }
           // 手前タグ種を「閉じタグ」に
-          beforeTagKind = KIND.TAG_CLOSE;
+          beforeElementKind = KIND.TAG_CLOSE;
           
           break;
           // -----------------------------------------------------
 
           // --------------------タグ以外の場合-------------------- 
         case KIND.CONTENT:
-          if (beforeTagKind === KIND.TAG_OPEN) {
+          if (beforeElementKind === KIND.TAG_OPEN) {
             // 手前タグが開きタグの場合
             const nextTag = this.getFirstElement(xmlTxt)
             if (nextTag.kind === KIND.TAG_CLOSE) {
@@ -99,21 +102,22 @@ export class ConvertService {
               if (currentObj[depth - 1] === null || currentObj[depth - 1] === undefined) {
                 currentObj[depth - 1] = {}
               }
-              let tmpObj = currentObj[depth - 1];
-              tmpObj[stackTop] = element.content;
+              // オブジェクト管理配列にキーとバリューを追加する
+              this.addKeyAndValue(currentObj, depth - 1, stackTop, element.content);
             } else {
               // 要素を<#text>タグでくくって戻す
               xmlTxt = '<#text>' + element.content + '</#text>' + xmlTxt;
             }
-          } else if (beforeTagKind === KIND.TAG_CLOSE) {
+          } else if (beforeElementKind === KIND.TAG_CLOSE) {
+            // 手前タグが閉じタグなら
             // 要素を<#text>タグでくくって戻す
             xmlTxt = '<#text>' + element.content + '</#text>' + xmlTxt;
           } else {
             // 処理はなし
-            console.log('CONTENT -> CONTENT');
+            console.log('ERROR: CONTENT -> CONTENT');
           }
           // 手前タグ種をなしに
-          beforeTagKind = KIND.CONTENT;
+          beforeElementKind = KIND.CONTENT;
 
           break;
           // -----------------------------------------------------
@@ -179,6 +183,30 @@ export class ConvertService {
         break
     }
     return kind;
+  }
+
+
+  // 指定のオブジェクト配列の指定の深さの部分に、キーとバリューのセットを追加する
+  private addKeyAndValue(objects: Array < object >, depth: number, key: string, value: string|object) {
+    let tmpObj = objects[depth];
+
+    if (tmpObj[key] === null || tmpObj[key] === undefined) {
+      // 追加先にキーが未定義ならそのまま追加
+      tmpObj[key] = value;
+    } else {
+      // 追加先にキーの値が定義されている場合
+      if (tmpObj[key] instanceof Array) {
+        // すでに配列になっていれば、バリューを追加するのみ
+        let tmpArray: Array <string|object> = tmpObj[key];
+        tmpArray.push(value);
+      } else {
+        // オブジェクトか文字列の場合
+        let tmpArray: Array <string|object> = [];
+        tmpArray.push(tmpObj[key]);
+        tmpArray.push(value);
+        tmpObj[key] = tmpArray;
+      }
+    }
   }
 
 }
